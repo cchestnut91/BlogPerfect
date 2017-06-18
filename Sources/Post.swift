d4d92446a0fad3c6425e350075c6a4f449223cc2
Calvin Chestnut <calvin_chestnut@me.com>
2017-06-18 17:25:39 -0400

    replace method again

--- Sources/Post.swift
+++ Sources/Post.swift
@@ -207,3 +207,27 @@ extension Post {
         return json
     }
 }
+
+// MARK: - HTML
+extension Post {
+
+    /// Inserts the post into HTML content given a template HTML string
+    ///
+    /// - Parameter templateString: The Template HTML string
+    /// - Returns: The updated HTML string with the post content
+    public func HTMLString(from templateString: String?) -> String {
+        var html = templateString ?? ""
+        
+        html = html.replacingOccurrences(of: "{POST_TITLE}", with: displayTitle())
+        html = html.replacingOccurrences(of: "{POST_PUBLISHED}", with: DateFormatter.titleFormatter().string(from: published))
+        html = html.replacingOccurrences(of: "{POST_SUMMARY}", with: summary ?? "")
+        html = html.replacingOccurrences(of: "{POST_AUTHOR}", with: author?.name ?? "")
+        html = html.replacingOccurrences(of: "{POST_URL}", with: url?.absoluteString ?? "")
+        html = html.replacingOccurrences(of: "{POST_MODIFIED}", with: modified != nil ? DateFormatter.standardFormatter().string(from: modified!) : "")
+        html = html.replacingOccurrences(of: "{POST_EXTERNAL_URL}", with: externalUrl?.absoluteString ?? "")
+        html = html.replacingOccurrences(of: "{POST_EXTERNAL_URL_TEXT}", with: externalUrlText ?? "")
+        html = html.replacingOccurrences(of: "{POST_CONTENT}", with: body?.markdownToHTML ?? "")
+        return html
+    }
+    
+}
